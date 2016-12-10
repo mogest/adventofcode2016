87d5bf6435865133f0e75eec9bd4dd1541476b2b
Roger Nesbitt <roger@seriousorange.com>
2016-12-10 13:51:26 +1300

    rewrite 09b using paco for a parser

--- 09b.exs
+++ 09b.exs
@@ -1,40 +1,42 @@
 defmodule Day09b do
+  import Paco
+  import Paco.Parser
+  alias Paco.ASCII
+
   def run do
-    IO.read(:all)
-    |> String.strip
-    |> String.graphemes
-    |> Enum.reduce({0, nil, []}, fn char, {output, command, multipliers} ->
-         cond do
-           command && char == ")" ->
-             {len, multiplier} = parse_command(command)
-             {output, nil, [{len, multiplier} | step(multipliers)]}
+    {:ok, tokens} = IO.read(:all) |> String.strip |> parse(parser)
 
-           command ->
-             {output, command <> char, step(multipliers)}
+    Enum.reduce(tokens, {0, []}, fn
+      [len, multiplier, string_length], {count, multipliers} ->
+        {count, [{len, multiplier} | step(string_length, multipliers)]}
 
-           char == "(" ->
-             {output, "", step(multipliers)}
-
-           true ->
-             {output + current_multiplier(multipliers), nil, step(multipliers)}
-         end
-       end)
+      _, {count, multipliers} ->
+        {count + current_multiplier(multipliers), step(1, multipliers)}
+    end)
     |> elem(0)
     |> IO.puts
   end
 
-  def parse_command(command) do
-    [_, len, multiplier] = Regex.run(~r/^(\d+)x(\d+)$/, command)
+  def parser do
+    number = while(ASCII.digit, at_least: 1) |> bind(&String.to_integer/1)
 
-    {String.to_integer(len), String.to_integer(multiplier)}
+    command =
+      number
+      |> separated_by("x")
+      |> surrounded_by(ASCII.round_brackets)
+      |> bind(fn [a, b] -> [a, b, String.length("#{a}#{b}") + 3] end)
+
+    repeat(one_of(ASCII.uppercase ++ [command]))
   end
 
   def current_multiplier(multipliers) do
     Enum.reduce(multipliers, 1, fn {_, multiplier}, acc -> acc * multiplier end)
   end
 
-  def step(multipliers) do
-    Enum.filter_map(
+  def step(0, multipliers), do: multipliers
+
+  def step(count, multipliers) do
+    step count - 1, Enum.filter_map(
       multipliers,
       fn {len, _} -> len > 0 end,
       fn {len, multiplier} -> {len - 1, multiplier} end
